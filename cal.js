30675f561622a1503394dfb70ff9a05619372904
som <somyaagrawal195@gmail.com>
2022-04-11 22:48:02 +0530

    final project

---
+++ cal.js
@@ -0,0 +1,42 @@
+let screen=document.getElementById('screen');
+buttons=document.querySelectorAll('button');
+let screenvalue='';
+for(item of buttons){
+    item.addEventListener('click',(e)=>{
+        buttonText=e.target.innerText;
+        console.log('button text is ',buttonText);
+        if(buttonText=='*'){
+            screenvalue +=buttonText;
+            screen.value=screenvalue;
+        }
+        else if(buttonText=='='){
+            screen.value=eval(screenvalue);
+
+        }
+        else if(buttonText=='C'){
+            screenvalue='';
+            screen.value=screenvalue;
+        }
+        else if(buttonText=='+'){
+            screenvalue +=buttonText;
+            screen.value=screenvalue;
+        }
+        else if(buttonText=='-'){
+            screenvalue +=buttonText;
+            screen.value=screenvalue;
+        }
+        else if(buttonText=='/'){
+            screenvalue +=buttonText;
+            screen.value=screenvalue;
+        }
+        else if(buttonText=='%'){
+            screenvalue +=buttonText;
+            screen.value=screenvalue;
+        }
+        else{
+            screenvalue +=buttonText;
+            screen.value=screenvalue;
+        }
+
+    })
+}
